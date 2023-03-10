644dc26c4f37aa2bb765de87ab4de6b9373a9e5f
Элиза <elayza@MacBook-Pro-Eliza.local>
2023-03-10 22:53:35 +0600

    hw

---
+++ app.js
@@ -0,0 +1,14 @@
+
+function ders(){
+    const num = ['apple -> elppa','grap -> parg']
+    console.log(num)
+}
+ders()
+
+function nbvs(){
+    const jol = [12,34,78,'=>'] 
+    const jik = [21,43,87]
+    const klj = [...jol, ...jik]
+    console.log(klj)
+}
+nbvs()
